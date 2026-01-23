a9d60a27b64f3834a9045ee5c8018115eba0c2c5
Andreas Johansson <andreas.johansson@chasacademy.se>
2026-01-23 14:50:22 +0100

    Removed translate Y on middle bar to fix centering issue

--- components/layout/BurgerButton.vue
+++ components/layout/BurgerButton.vue
@@ -9,28 +9,27 @@ defineEmits<{ (e: 'toggle'): void }>()
     class="relative w-10 h-10 text-white"
     @click="$emit('toggle')"
   >
-    <!-- Top bar -->
-    <span
-      class="absolute left-1/2 top-1/2 w-10 h-0.5 bg-current transition-transform duration-200"
-      :class="open
-        ? 'rotate-45 -translate-x-1/2 -translate-y-1/2'
-        : '-translate-x-1/2 -translate-y-3'"
-    />
+<!-- Top bar -->
+<span
+  class="absolute left-1/2 top-1/2 w-10 h-0.5 bg-current transition-transform duration-200"
+  :class="open
+    ? 'rotate-45 -translate-x-1/2 -translate-y-1/2'
+    : '-translate-x-1/2 -translate-y-3'"
+/>
 
-    <!-- Middle bar -->
-    <span
-      class="absolute left-1/2 top-1/2 w-10 h-0.5 bg-current transition-opacity duration-200"
-      :class="open
-        ? 'opacity-0'
-        : '-translate-x-1/2 -translate-y-1/2'"
-    />
+<!-- Middle bar -->
+<span
+  class="absolute left-1/2 top-1/2 w-10 h-0.5 bg-current transition-opacity duration-200
+         -translate-x-1/2"
+  :class="open ? 'opacity-0' : ''"
+/>
 
-    <!-- Bottom bar -->
-    <span
-      class="absolute left-1/2 top-1/2 w-10 h-0.5 bg-current transition-transform duration-200"
-      :class="open
-        ? '-rotate-45 -translate-x-1/2 -translate-y-1/2'
-        : '-translate-x-1/2 translate-y-3'"
-    />
+<!-- Bottom bar -->
+<span
+  class="absolute left-1/2 top-1/2 w-10 h-0.5 bg-current transition-transform duration-200"
+  :class="open
+    ? '-rotate-45 -translate-x-1/2 -translate-y-1/2'
+    : '-translate-x-1/2 translate-y-3'"
+/>
   </button>
 </template>
