787ed500e91dd0d737beb798f1612278cd4c5cef
Henrik Suurik <henrik.suurik@chasacademy.se>
2026-02-07 16:45:15 +0100

    Increased maxLines to 8 (needed for real iPhone SE Gen3)

--- pages/services/printed-matter.vue
+++ pages/services/printed-matter.vue
@@ -38,7 +38,7 @@ useHead({
 
 // ===== STATE (ref/reactive) =====
 const servicesData = ref<IServiceCardContent[]>(
-  (services as IServiceCardContent[]).map(item => ({ ...item, maxLines: 7 }))
+  (services as IServiceCardContent[]).map(item => ({ ...item, maxLines: 8 }))
 )
 const itemsPerPage = ref(1)
 
