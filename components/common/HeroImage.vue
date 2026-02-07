f0906e6f0ec0b2f38228d2514c966b53a7c5581b
Henrik Suurik <henrik.suurik@chasacademy.se>
2026-02-07 15:29:06 +0100

    Removed test for Older Safari devices

--- components/common/HeroImage.vue
+++ components/common/HeroImage.vue
@@ -43,7 +43,6 @@ const props = withDefaults(defineProps<Props>(), {
 })
 
 // ===== COMPUTED =====
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const aspectRatio = computed(() => {
   // Prevent division by zero
   if (!props.width || !props.height) return 3 / 2
@@ -54,6 +53,7 @@ const aspectRatio = computed(() => {
 <template>
   <section
     class="container-section"
+    :style="{ aspectRatio: aspectRatio }"
   >
     <!-- Hero Image -->
     <NuxtImg
