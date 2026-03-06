9df35e2742000754a725d188eac985cce2be8395
Henrik Suurik <henrik.suurik@chasacademy.se>
2026-03-06 16:32:05 +0100

    Fixed hydration issue when running on local machine

--- components/common/ImageCard.vue
+++ components/common/ImageCard.vue
@@ -68,7 +68,7 @@ const handleClick = (): void => {
         format="webp"
         :alt="alt"
         quality="80"
-        sizes="xs:100vw sm:50vw lg:33vw xl:25vw"
+        sizes="100vw sm:50vw lg:33vw xl:25vw"
         fit="cover"
         loading="lazy"
         fetchpriority="low"
