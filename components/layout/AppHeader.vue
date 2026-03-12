09db2ea33b6e41deb8b9bd313b3e6013b8616153
Henrik Suurik <henrik.suurik@chasacademy.se>
2026-03-12 14:23:44 +0100

    Remove overflow hidden from header and adjust width classes for layout element

--- components/layout/AppHeader.vue
+++ components/layout/AppHeader.vue
@@ -26,7 +26,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <header class="overflow-hidden min-h-[80px] bg-neutral-900 sticky top-0 z-50 shadow-[0_4px_10px_rgba(0,0,0,0.25)] shadow-black/50 transition-all duration-300" :class="scrolled ? 'min-h-[60px]' : 'min-h-[80px]'">
+  <header class="min-h-[80px] bg-neutral-900 sticky top-0 z-50 shadow-[0_4px_10px_rgba(0,0,0,0.25)] shadow-black/50 transition-all duration-300" :class="scrolled ? 'min-h-[60px]' : 'min-h-[80px]'">
     <div class="min-w-[100px] mx-auto max-w-full px-3 lg:px-5 flex items-center justify-between">
       <!-- Logo -->
       <NuxtLink
@@ -66,7 +66,7 @@ onMounted(() => {
       <!-- Dummy layout element -->
       <div
         class="hidden sm:block transition-all duration-300"
-        :class="scrolled ? 'min-w-[60px] min-h-[60px]' : 'min-w-[100px] min-h-[100px]'"
+        :class="scrolled ? 'w-[60px] min-h-[60px]' : 'w-[100px] min-h-[100px]'"
         aria-hidden="true"
       />
 
