6fd86d89a4744361e5f5e353a4131566fa10c3e5
Andreas Johansson <andreas.johansson@chasacademy.se>
2026-03-30 13:28:23 +0200

    192-canvas-background-is-deleted-after-form-is-sumbitted (#198)
    * Added clear objects method to canvas store. Added watcher to canvaspanel to clear objects when sequencer is increased
    
    * Changed clearObjects to not automatically set the activeSide to 0 and instead do it manually from quoteform

--- components/features/QuoteForm.vue
+++ components/features/QuoteForm.vue
@@ -267,7 +267,8 @@ async function handleSubmit(): Promise<void> {
     emit('success')
 
     // Clear canvases immediately as it takes some time to clear the canvas object
-    canvasStore.clear()
+    canvasStore.setActiveSide(0)
+    canvasStore.clearObjects()
 
     // Auto-hide success message after 5 seconds then reset user-editable fields
     setTimeout(() => {
